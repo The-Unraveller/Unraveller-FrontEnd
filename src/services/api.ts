e54bed964950e3822d50b7e0fced14a62da97e03
ShanKhao <shankhao2005@gmail.com>
2026-05-25 07:09:52 +0700

    undate services api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5251/api';
+const API_BASE_URL = 'https://localhost:7055/api';
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
